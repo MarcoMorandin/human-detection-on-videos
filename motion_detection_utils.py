d2222ca800e9cc32e2489e284bca2356f793653c
lorenzoArcangeli <lorenz.arcangeli@studenti.unicam.it>
2024-12-27 12:32:57 +0100

    added-edit-function-fot-people-proportion

--- motion_detection_utils.py
+++ motion_detection_utils.py
@@ -39,6 +39,33 @@ def get_contour_detections(mask, thresh=400):
 
 
 
+def get_contour_detections_edit(mask, thresh=400):
+    """ Obtains initial proposed detections from contours discoverd on the
+        mask. Scores are taken as the bbox area, larger is higher.
+        Inputs:
+            mask - thresholded image mask
+            thresh - threshold for contour size
+        Outputs:
+            detectons - array of proposed detection bounding boxes and scores 
+                        [[x1,y1,x2,y2,s]]
+        """
+    # get mask contours
+    contours, _ = cv2.findContours(mask, 
+                                   cv2.RETR_EXTERNAL, # cv2.RETR_TREE, 
+                                   cv2.CHAIN_APPROX_TC89_L1)
+    detections = []
+    filtered_contours = []
+    for cnt in contours:
+        x,y,w,h = cv2.boundingRect(cnt)
+        area = w*h
+        if area > thresh: # hyperparameter
+            detections.append([x,y,x+w,y+h, area])
+            filtered_contours.append(cnt)
+
+    return np.array(detections), filtered_contours
+
+
+
 # =============================================================================
 # Non-Max Supression for detected bounding boxes on blobs
 
@@ -51,7 +78,7 @@ def remove_contained_bboxes(boxes):
         Outputs:
             keep - indexes of bounding boxes that are not entirely contained 
                    in another box
-        """
+    """
     check_array = np.array([True, True, False, False])
     keep = list(range(0, len(boxes)))
     for i in keep: # range(0, len(bboxes)):
@@ -99,6 +126,57 @@ def non_max_suppression(boxes, scores, threshold=1e-1):
                 order.remove(j)
                 
     return boxes[keep]
+
+
+def non_max_suppression_edit(boxes, scores, contours, threshold=1e-1):
+    """
+    Perform non-max suppression on a set of bounding boxes 
+    and corresponding scores.
+    Inputs:
+        boxes: a list of bounding boxes in the format [xmin, ymin, xmax, ymax]
+        scores: a list of corresponding scores 
+        threshold: the IoU (intersection-over-union) threshold for merging bboxes
+    Outputs:
+        boxes - non-max suppressed boxes
+    """
+
+    # Sort the boxes and contours by score in descending order
+    order = np.argsort(scores)[::-1]
+    boxes = boxes[order]
+    contours=contours[order]
+    #contours = [contours[i] for i in order]
+
+    # Remove all contained bounding boxes and get ordered indices
+    keep_indices = remove_contained_bboxes(boxes)
+
+    # Filter boxes and contours based on `keep_indices`
+    boxes = boxes[keep_indices]
+    contours[keep_indices]
+    #contours = [contours[i] for i in keep_indices]
+
+    keep = []
+    #final_contours = []
+
+    while order:
+
+        i = order.pop(0)
+        keep.append(i)
+        for j in order:
+            # Calculate the IoU between the two boxes
+            intersection = max(0, min(boxes[i][2], boxes[j][2]) - max(boxes[i][0], boxes[j][0])) * \
+                           max(0, min(boxes[i][3], boxes[j][3]) - max(boxes[i][1], boxes[j][1]))
+            union = (boxes[i][2] - boxes[i][0]) * (boxes[i][3] - boxes[i][1]) + \
+                    (boxes[j][2] - boxes[j][0]) * (boxes[j][3] - boxes[j][1]) - intersection
+            iou = intersection / union
+
+            # Remove boxes with IoU greater than the threshold
+            if iou > threshold:
+                order.remove(j)
+
+    for i in keep:
+        cv2.contourArea(contours[i])
+
+    return boxes[keep], contours[keep]
 
 
 # =============================================================================
