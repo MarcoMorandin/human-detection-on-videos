bfa7b56b5aa019e2d4094d2b9b115a517f034766
marcomorandin <emmeemmenove@gmail.com>
2025-01-12 12:59:00 +0100

    fixed multithreading

---
+++ server.py
@@ -0,0 +1,112 @@
+import cv2
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+from datetime import datetime
+import time
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from Detector import HumanDetector
+from OpticalFlowDetector import OpticalFlowTracking
+from threading import Event, Thread
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+clients = {}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def video_stream(sid):
+    cap = cv2.VideoCapture(clients[sid]["video"])
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    if not cap.isOpened():
+        print(f"[{sid}] Error: Could not open video file.")
+        return
+    
+    humanDetector = None
+    first_frame = True
+    old_frame=None
+
+    while cap.isOpened():
+        if clients[sid]["stop_event"].is_set():
+            print(f"[{sid}] Stopping current video stream.")
+            cap.release()
+            return
+        
+        if first_frame:
+            first_frame = False
+            ret, old_frame = cap.read()
+            if not ret:
+                print(f"[{sid}] Error: Could not read the first frame.")
+            humanDetector_opticalflow= OpticalFlowTracking()
+            humanDetector = HumanDetector(old_frame)
+            continue
+        
+        ret, frame = cap.read()
+        if not ret:
+            print(f"[{sid}] End of video or read error. Restarting video stream.")
+            cap.release()
+            return    
+        try:
+            if clients[sid]["stop_event"].is_set():
+                cap.release()
+                return
+            
+            boxes_of = humanDetector_opticalflow.detect_humans(old_frame, frame)
+            frame_edited, boxes = humanDetector.detect_humans(frame, boxes_of, overlap_boxes_treshold=0.7)
+            socketio.emit('n-boxes', len(boxes), to=sid)
+            _, encoded_frame = cv2.imencode('.jpg', frame_edited)
+            frame_data = encoded_frame.tobytes()
+            socketio.emit('frame', frame_data, to=sid)
+        except Exception as e:
+            print(f"[{sid}] Error during detection: {e}")
+        old_frame=frame
+        time.sleep(1/fps)
+    
+    
+    cap.release()
+
+@socketio.on('connect')
+def handle_connect():
+    sid = request.sid
+    print(f"[{sid}] Client connected at {datetime.now()}")
+    clients[sid] = {
+        "thread": socketio.start_background_task(video_stream, sid),
+        "stop_event": Event(),
+        "video": "in.avi"
+    }
+    
+@socketio.on('message')
+def handle_message(data):
+    sid = request.sid
+    video = data
+    print(f"[{sid}] Message from client: {data}")
+
+    if clients[sid]["thread"]:
+        clients[sid]["stop_event"].set()
+        clients[sid]["thread"].join()
+
+    clients[sid]["stop_event"].clear()
+    clients[sid]["video"] = video
+    clients[sid]["thread"] = socketio.start_background_task(video_stream, sid)
+    clients[sid]["thread"].start()
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    sid = request.sid
+    print(f"[{sid}] Client disconnected at {datetime.now()}")
+
+    if sid in clients:
+        clients[sid]["stop_event"].set()
+        if clients[sid]["thread"]:
+            clients[sid]["thread"].join()
+        del clients[sid]
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
