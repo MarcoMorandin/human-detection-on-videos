4da8d8cf0dcf2e097d995182e1a554f4b5c2b6cb
marcomorandin <emmeemmenove@gmail.com>
2025-01-14 19:23:01 +0100

    add comments

--- server.py
+++ server.py
@@ -7,24 +7,34 @@ from Detector import HumanDetector
 from OpticalFlowDetector import OpticalFlowTracking
 from threading import Event
 
+# Initialize Flask app and SocketIO
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
+# Dictionary to store connected client information
 clients = {}
 
 @app.route('/')
 def index():
+    # Render the main page of the web app
     return render_template('index.html')
 
 def video_stream(sid):
+    # Check if client exists
     if(sid not in clients):
         print(f"[{sid}] Error: Client not found.")
         return
+    
+    # Open video file (only for simulation purposes, in real life this would be a video stream)
+    # if the client has not specified a video file, use the default one
     if("video" not in clients[sid]):
         cap = cv2.VideoCapture("in.avi")    
     else:
         cap = cv2.VideoCapture(clients[sid]["video"])
+        
     fps = cap.get(cv2.CAP_PROP_FPS)
+    
+    # Check if the video could be opened otherwise exit
     if not cap.isOpened():
         print(f"[{sid}] Error: Could not open video file.")
         return
@@ -33,12 +43,15 @@ def video_stream(sid):
     first_frame = True
     old_frame=None
 
+    # Read the video frame by frame until the end of the video
     while cap.isOpened():
+        # Check if the client has requested to stop the video stream
         if clients[sid]["stop_event"].is_set():
             print(f"[{sid}] Stopping current video stream.")
             cap.release()
             return
         
+        # Read the first frame and initialize the human detector (the first frame is used as a background)
         if first_frame:
             first_frame = False
             ret, old_frame = cap.read()
@@ -54,13 +67,18 @@ def video_stream(sid):
             cap.release()
             return    
         try:
+            # Check if the client has requested to stop the video stream while processing
             if clients[sid]["stop_event"].is_set():
                 cap.release()
                 return
             
+            # Get the bounding boxes of humans in the current frame detected by the optical flow
             boxes_of = humanDetector_opticalflow.detect_humans(old_frame, frame)
+            # Get the frame with the bounding boxes drawned and the bounding boxes
             frame_edited, boxes = humanDetector.detect_humans(frame, boxes_of, overlap_boxes_treshold=0.7)
+            # Send the number of bounding boxes to the client
             socketio.emit('n-boxes', len(boxes), to=sid)
+            # Encode the frame and send it to the client
             _, encoded_frame = cv2.imencode('.jpg', frame_edited)
             frame_data = encoded_frame.tobytes()
             socketio.emit('frame', frame_data, to=sid)
@@ -72,37 +90,46 @@ def video_stream(sid):
     
     cap.release()
 
+# SocketIO event handlers for client connections
 @socketio.on('connect')
 def handle_connect():
     sid = request.sid
     print(f"[{sid}] Client connected at {datetime.now()}")
+    
+    # Add client to the dictionary
     clients[sid] = {
         "thread": None,
         "stop_event": Event(),
         "video": "in.avi"
     }
     
+    # Start the video stream for the client
     clients[sid]["thread"] = socketio.start_background_task(video_stream, sid)
     
+# SocketIO event handlers for client messages
 @socketio.on('message')
 def handle_message(data):
     sid = request.sid
     video = data
     print(f"[{sid}] Message from client: {data}")
 
+    # Stop the current video stream if it is running
     if clients[sid]["thread"]:
         clients[sid]["stop_event"].set()
         clients[sid]["thread"].join()
 
+    # Start the video stream with the new video file
     clients[sid]["stop_event"].clear()
     clients[sid]["video"] = video
     clients[sid]["thread"] = socketio.start_background_task(video_stream, sid)
 
+# SocketIO event handlers for client disconnections
 @socketio.on('disconnect')
 def handle_disconnect():
     sid = request.sid
     print(f"[{sid}] Client disconnected at {datetime.now()}")
 
+    # Clean up client information
     if sid in clients:
         clients[sid]["stop_event"].set()
         if clients[sid]["thread"]:
